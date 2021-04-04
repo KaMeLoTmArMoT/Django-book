12c702f6c88d279ce72b133836dbc3235954cae3
KaMeLoTmArMoT <andyshebeko@gmail.com>
2021-04-05 02:34:58 +0300

    ch7

--- users/views.py
+++ users/views.py
@@ -8,4 +8,4 @@ from .forms import CustomUserCreationForm
 class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'account/signup.html'
